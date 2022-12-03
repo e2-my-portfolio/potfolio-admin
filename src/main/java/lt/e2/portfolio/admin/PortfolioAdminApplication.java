07beae11c541f7e900a329983f4e57f517300bb7
Dum6o <edwardasas@gmail.com>
2022-12-03 09:40:25 +0200

    project init

---
+++ src/main/java/lt/e2/portfolio/admin/PortfolioAdminApplication.java
@@ -0,0 +1,13 @@
+package lt.e2.portfolio.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioAdminApplication.class, args);
+	}
+
+}
